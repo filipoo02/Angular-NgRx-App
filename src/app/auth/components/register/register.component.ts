165488a700020f9ee85d5738c094a7e69a503175
filipoo02 <filipsadowski.os@o2.pl>
2022-01-23 14:21:09 +0100

    isSubmiting selector

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,34 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 
-import { registerAction } from '../../store/actions'
+import { registerAction } from '../../store/actions';
+import { AppStateInterface } from '../../../shared/types/appState.interface';
+import { Observable } from 'rxjs';
+import { isSubmittingSelector } from '../../store/selectors';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup;
+  isSubmitting$: Observable<boolean>;
 
-  constructor(private fb: FormBuilder, private store: Store) { }
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<AppStateInterface>
+  ) {}
 
   ngOnInit(): void {
-    this.initilizeForm()
+    this.initilizeForm();
+    this.initilizeValues();
   }
 
-  initilizeForm(): void{
+  initilizeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
+  }
+
+  initilizeForm(): void {
     this.form = this.fb.group({
       username: ['', Validators.required],
       email: ['', Validators.required],
       password: ['', Validators.required],
-    })
+    });
   }
 
-  onSubmit(): void{
+  onSubmit(): void {
     console.log(this.form.value);
-    this.store.dispatch(registerAction(this.form.value))
+    this.store.dispatch(registerAction(this.form.value));
   }
-
 }
